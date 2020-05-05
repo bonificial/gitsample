8da417b99de53c92f365ca609f512bee4cf537e1
AMS-JR <asj.sarjo@gmail.com>
2020-05-06 03:04:44 +0530

    booking timings
    made some additions to the database and also added some models for topics and timings

---
+++ app/models/topics/categoriesmodel.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'config/dbhmodel.php';
+
+class Categories extends Dbh{
+    
+    public $id_category;
+    public $name;
+        
+}
---
+++ app/models/topics/timingsmodel.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'config/dbhmodel.php';
+
+class Timings extends Dbh{
+    
+    public $timings_id;
+    public $start_from;
+    public $end_at;
+    public $id_topic;
+        
+}
