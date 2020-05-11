7e21fb7788b591110f59a72cf2fae84742281e9f
AMS-JR <asj.sarjo@gmail.com>
2020-05-12 03:07:42 +0530

    create profile
    the profile the add when signing up but with empty data except the id_user field

--- app/models/profile/profilemodel.php
+++ app/models/profile/profilemodel.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'config/dbhmodel.php';
+include_once 'config/dbh.php';
 
 class Profile extends Dbh{
     
@@ -10,4 +10,12 @@ class Profile extends Dbh{
     public $ratings;
     public $hourly_price;
     public $id_user;
+
+    public function create_profile($id_user){
+        $sql = "INSERT INTO profile (id_user) VALUES(?)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$id_user]);
+        return array('message' => 'account successfully created!','status' => 'success');
+        $stmt = null;
+    }
 }
