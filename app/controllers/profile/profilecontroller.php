c8b9a8e44e44050496178466daf194aa6f11ec34
AMS-JR <asj.sarjo@gmail.com>
2020-05-21 05:53:33 +0530

    update profile

--- app/controllers/profile/profilecontroller.php
+++ app/controllers/profile/profilecontroller.php
@@ -4,4 +4,12 @@
 include_once 'app/models/profile/profilemodel.php';
 class ProfileController extends Profile{
 
+    public function index(){
+        
+    }
+    public function update($request, $response, $args){
+        $response->getBody()->write(json_encode($this->update_profile($request->getParsedBody())));
+        return $response
+                ->withHeader('Content-Type', 'application/json');
+    }
 }
