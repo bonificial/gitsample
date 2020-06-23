7f3eb27946d5b118f1b11f00c8a334be417fc2c1
AMS-JR <asj.sarjo@gmail.com>
2020-06-23 21:25:29 +0000

    profile edits done

--- app/controllers/profile/profilecontroller.php
+++ app/controllers/profile/profilecontroller.php
@@ -2,21 +2,34 @@
 <?php
 
 include_once 'app/models/profile/profilemodel.php';
+include_once 'assets/validator.php';
 class ProfileController extends Profile{
+    private $validator;
 
+    public function __construct(){
+        $this->validator = new Validator();
+    }
     public function index($request, $response, $args){
         $response->getBody()->write(json_encode($this->get_profiles()));
         return $response
                 ->withHeader('Content-Type', 'application/json');
     }
     public function update($request, $response, $args){
-        $response->getBody()->write(json_encode($this->update_profile($request->getParsedBody())));
+        $profileInfo = $this->validator::validate_profileUpdate($request->getParsedBody());
+        if($this->validator::$errors != null)
+             $response->getBody()->write(json_encode($this->_rename_arr_key()));
+        else
+             $response->getBody()->write(json_encode($this->update_profile($profileInfo)));
         return $response
                 ->withHeader('Content-Type', 'application/json');
     }
     public function show($request, $response, $args){
-            $response->getBody()->write(json_encode($this->get_profile($args['id_profile'])));
-            return $response
-                    ->withHeader('Content-Type', 'application/json');
+        $response->getBody()->write(json_encode($this->get_profile($args['id_profile'])));
+        return $response
+                ->withHeader('Content-Type', 'application/json');
+    }
+    public function _rename_arr_key() {
+        $key = array_keys($this->validator::$errors);
+        return array('message' => $this->validator::$errors[$key[0]],'status' => 'error');
     }
 }
